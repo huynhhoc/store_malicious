5add588aa8bcf7f28f4d2133a775ee54c68a820d
huynhhoc <huynhhoc@gmail.com>
2023-11-15 19:43:46 +0100

    update unit test and integration test

---
+++ TestIOCProcessor.py
@@ -0,0 +1,46 @@
+# Author: Huynh Thai Hoc
+import unittest
+from unittest.mock import patch, Mock, MagicMock
+from ioc_processor import extract_urls_ips, insert_data, create_tables
+
+class TestIOCProcessor(unittest.TestCase):
+    # This test mocks the 'requests.get' method to simulate a response
+    @patch('requests.get')
+    def test_extract_urls_ips(self, mock_requests_get):
+        # Mock the response from requests.get
+        mock_response = Mock()
+        mock_response.status_code = 200
+        mock_response.text = "http://103.29.2.134/a-r.m-5.Sakura, http://yashoda.brandwizz.in/netTimer.exe online, 123.29.2.124"
+        mock_requests_get.return_value = mock_response
+
+        # Call the function with is_content=True
+        urls, ips, contents_urls, contents_ips = extract_urls_ips(mock_response, is_content=True)
+        
+        # Assert that the function returns the expected results
+        self.assertEqual(urls, ["http://103.29.2.134/a-r.m-5.Sakura", "http://yashoda.brandwizz.in/netTimer.exe"])
+        self.assertEqual(ips, ["123.29.2.124"])
+        self.assertEqual("http://103.29.2.134/a-r.m-5.Sakura, http://yashoda.brandwizz.in/netTimer.exe online, 123.29.2.124", contents_urls[0])
+        self.assertEqual("http://103.29.2.134/a-r.m-5.Sakura, http://yashoda.brandwizz.in/netTimer.exe online, 123.29.2.124", contents_urls[1])
+        self.assertEqual("http://103.29.2.134/a-r.m-5.Sakura, http://yashoda.brandwizz.in/netTimer.exe online, 123.29.2.124", contents_ips[0])
+
+    # This test mocks the 'psycopg2.connect' method to simulate a database connection
+    @patch('psycopg2.connect')
+    def test_create_tables(self, mock_connect):
+        # Mock the database connection
+        mock_conn = MagicMock()
+        mock_connect.return_value = mock_conn
+
+        # Mock the cursor and execute methods
+        mock_cursor = MagicMock()
+        mock_conn.cursor.return_value = mock_cursor
+        mock_execute = MagicMock()
+        mock_cursor.execute = mock_execute
+        
+        # Call the function
+        is_success = create_tables(log=None)
+        
+        # Assert that the function returns True, indicating success
+        self.assertEqual(is_success, True)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ TestInsertDataIntegrationURLs.py
@@ -0,0 +1,59 @@
+# Author: Huynh Thai Hoc
+import unittest
+import psycopg2
+from ioc_processor import insert_data
+from utils.config import DB_CONFIG
+
+class TestInsertDataIntegrationURLs(unittest.TestCase):
+    # setUp is called before each test method to set up necessary resources
+    def setUp(self):
+        # Set up a test database and connection
+        self.test_conn = psycopg2.connect(**DB_CONFIG)
+        self.test_cursor = self.test_conn.cursor()
+
+        # Create the necessary tables for testing
+        self.test_cursor.execute('''
+            CREATE TABLE IF NOT EXISTS urls (
+                id SERIAL PRIMARY KEY,
+                url TEXT UNIQUE,
+                content TEXT,
+                source TEXT
+            )
+        ''')
+        self.test_conn.commit()
+
+    # tearDown is responsible for cleaning up resources after each test
+    def tearDown(self):
+        try:
+            # Clean up the test database after each test
+            self.test_cursor.execute("DROP TABLE IF EXISTS urls")
+            self.test_conn.commit()
+        except Exception:
+            # If an exception occurs during cleanup, roll back the transaction
+            self.test_conn.rollback()
+        finally:
+            self.test_conn.close()
+
+    # test_insert_data_urls_integration is the actual test method for 'urls'
+    def test_insert_data_urls_integration(self):
+        # Sample data for the 'urls' table
+        urls = ["http://example.com", "http://anotherexample.com"]
+        contents_urls = ["content1", "content2"]
+        source_url = "source_url"
+
+        # Call the function to insert data into 'urls' table
+        insert_data(urls, contents_urls, source_url, 'urls', log=None)
+
+        # Verify the data in the test database
+        self.test_cursor.execute("SELECT * FROM urls")
+        result = self.test_cursor.fetchall()
+
+        # Assert that the inserted data matches the expected result
+        expected_result = [
+            (1, 'http://example.com', 'content1', 'source_url'),
+            (2, 'http://anotherexample.com', 'content2', 'source_url')
+        ]
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
